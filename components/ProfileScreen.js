06d6df5a9a2da174b147d83054088b316741359b
Josh W <jtwong10@gmail.com>
2020-03-23 16:39:19 -0700

    implemented loading animation while querying firebase

--- components/ProfileScreen.js
+++ components/ProfileScreen.js
@@ -19,7 +19,7 @@ export default class ProfileScreen extends React.Component {
     this.state = {
       user: {},
       userInfo: {},
-      profilePicture: "",
+      profilePicture: "https://miro.medium.com/max/1080/0*DqHGYPBA-ANwsma2.gif",
       description: "",
       userDescription: ""
     };
