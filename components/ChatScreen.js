bd3c8b3e121d17bb6ce7a16103af68fed2e124d2
Josh W <jtwong10@gmail.com>
2020-04-02 00:05:26 -0700

    updated componentdidmount method

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -11,13 +11,19 @@ import {
 
 import Firebase from "../config/Firebase";
 
+import * as geolib from "geolib";
+
 export default class ProfileScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       user: {},
-      userInfo: {}
+      userInfo: {},
+      currentUserLat: "",
+      currentUserLon: "",
+      matchedUsers: []
     };
+    this.checkMatches = this.checkMatches.bind(this);
   }
 
   componentDidMount() {
@@ -32,6 +38,16 @@ export default class ProfileScreen extends React.Component {
           console.log("No user");
         } else {
           this.setState({ userInfo: doc.data() });
+
+          let currentUserLocation = this.state.userInfo.locationCoordinates;
+          let currentUserLocationSplit = currentUserLocation.split(",");
+          let currentUserLat = currentUserLocationSplit[0];
+          let currentUserLon = currentUserLocationSplit[1];
+
+          this.setState({
+            currentUserLat: currentUserLat,
+            currentUserLon: currentUserLon
+          });
         }
       })
       .catch(error => {
@@ -39,12 +55,72 @@ export default class ProfileScreen extends React.Component {
       });
 
     this.setState({ user: user, userInfo: userInfo });
+
+    let userlist = [];
+    let allUserRef = Firebase.firestore().collection("users");
+
+    allUserRef
+      .get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          let userInfo = this.state.userInfo;
+
+          console.log(doc.id, "=>", doc.data());
+
+          let locationCoordinates = doc.data().locationCoordinates;
+          let locationSplit = locationCoordinates.split(",");
+          let targetUserLat = locationSplit[0];
+          let targetUserLon = locationSplit[1];
+
+          var targetUser = {
+            latitude: targetUserLat,
+            longitude: targetUserLon
+          };
+
+          var currentUser = {
+            latitude: this.state.currentUserLat,
+            longitude: this.state.currentUserLon
+          };
+
+          var distanceBetween =
+            geolib.getPreciseDistance(currentUser, targetUser, 100) / 1000;
+
+          let tempUser = {
+            email: doc.data().email,
+            name: doc.data().name,
+            description: doc.data().description,
+            distance: distanceBetween,
+            imageSource: doc.data().imageSource,
+            swipedYes: doc.data().swipedYes
+          };
+
+          console.log(tempUser);
+
+          if (tempUser.swipedYes.indexOf(userInfo.email) > -1) {
+            userlist.push(tempUser);
+          }
+        });
+        this.setState({ matchedUsers: userlist });
+        console.log("list matched users here:", userlist);
+      })
+      .catch(error => {
+        console.log("Error here", error);
+      });
   }
 
+  checkMatches() {}
+
   render() {
+    var matchedUsers = [];
+    for (let i = 0; i < this.state.matchedUsers.length; i++) {
+      matchedUsers.push(this.state.matchedUsers[i]);
+    }
+    console.log(matchedUsers);
+
     return (
       <View style={styles.container}>
         <Text>Hi {this.state.userInfo.name}, this is where you chat</Text>
+
         <Button
           title="Match"
           onPress={() => this.props.navigation.navigate("Match")}
